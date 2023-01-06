8e93b6736d5424700ee16109300a004d7faf909b
gmurad97 <murad.dev@bk.ru>
2023-01-06 23:07:06 +0400

    release

--- script/script.js
+++ script/script.js
@@ -1,108 +1,142 @@
 $(document).ready(function () {
     $("#cur-alias").click(function () {
         $("body").css("cursor", "alias");
+        $(".cursor-label").text("cursor: alias;");
     });
     $("#cur-all-scroll").click(function () {
         $("body").css("cursor", "all-scroll");
+        $(".cursor-label").text("cursor: all-scroll;");
     });
     $("#cur-auto").click(function () {
         $("body").css("cursor", "auto");
+        $(".cursor-label").text("cursor: auto;");
     });
     $("#cur-cell").click(function () {
         $("body").css("cursor", "cell");
+        $(".cursor-label").text("cursor: cell;");
     });
     $("#cur-col-resize").click(function () {
         $("body").css("cursor", "col-resize");
+        $(".cursor-label").text("cursor: col-resize;");
     });
     $("#cur-context-menu").click(function () {
         $("body").css("cursor", "context-menu");
+        $(".cursor-label").text("cursor: context-menu;");
     });
     $("#cur-copy").click(function () {
         $("body").css("cursor", "copy");
+        $(".cursor-label").text("cursor: copy;");
     });
     $("#cur-crosshair").click(function () {
         $("body").css("cursor", "crosshair");
+        $(".cursor-label").text("cursor: crosshair;");
     });
     $("#cur-default").click(function () {
         $("body").css("cursor", "default");
+        $(".cursor-label").text("cursor: default;");
     });
     $("#cur-e-resize").click(function () {
         $("body").css("cursor", "e-resize");
+        $(".cursor-label").text("cursor: e-resize;");
     });
     $("#cur-ew-resize").click(function () {
         $("body").css("cursor", "ew-resize");
+        $(".cursor-label").text("cursor: ew-resize;");
     });
     $("#cur-grab").click(function () {
         $("body").css("cursor", "grab");
+        $(".cursor-label").text("cursor: grab;");
     });
     $("#cur-grabbing").click(function () {
         $("body").css("cursor", "grabbing");
+        $(".cursor-label").text("cursor: grabbing;");
     });
     $("#cur-help").click(function () {
         $("body").css("cursor", "help");
+        $(".cursor-label").text("cursor: help;");
     });
     $("#cur-move").click(function () {
         $("body").css("cursor", "move");
+        $(".cursor-label").text("cursor: move;");
     });
     $("#cur-n-resize").click(function () {
         $("body").css("cursor", "n-resize");
+        $(".cursor-label").text("cursor: n-resize;");
     });
     $("#cur-ne-resize").click(function () {
         $("body").css("cursor", "ne-resize");
+        $(".cursor-label").text("cursor: ne-resize;");
     });
     $("#cur-nesw-resize").click(function () {
         $("body").css("cursor", "nesw-resize");
+        $(".cursor-label").text("cursor: nesw-resize;");
     });
     $("#cur-ns-resize").click(function () {
         $("body").css("cursor", "ns-resize");
+        $(".cursor-label").text("cursor: ns-resize;");
     });
     $("#cur-nw-resize").click(function () {
         $("body").css("cursor", "nw-resize");
+        $(".cursor-label").text("cursor: nw-resize;");
     });
     $("#cur-nwse-resize").click(function () {
         $("body").css("cursor", "nwse-resize");
+        $(".cursor-label").text("cursor: nwse-resize;");
     });
     $("#cur-no-drop").click(function () {
         $("body").css("cursor", "no-drop");
+        $(".cursor-label").text("cursor: no-drop;");
     });
     $("#cur-none").click(function () {
         $("body").css("cursor", "none");
+        $(".cursor-label").text("cursor: none;");
     });
     $("#cur-not-allowed").click(function () {
         $("body").css("cursor", "not-allowed");
+        $(".cursor-label").text("cursor: not-allowed;");
     });
     $("#cur-pointer").click(function () {
         $("body").css("cursor", "pointer");
+        $(".cursor-label").text("cursor: pointer;");
     });
     $("#cur-progress").click(function () {
         $("body").css("cursor", "progress");
+        $(".cursor-label").text("cursor: progress;");
     });
     $("#cur-row-resize").click(function () {
         $("body").css("cursor", "row-resize");
+        $(".cursor-label").text("cursor: row-resize;");
     });
     $("#cur-s-resize").click(function () {
         $("body").css("cursor", "s-resize");
+        $(".cursor-label").text("cursor: s-resize;");
     });
     $("#cur-se-resize").click(function () {
         $("body").css("cursor", "se-resize");
+        $(".cursor-label").text("cursor: se-resize;");
     });
     $("#cur-sw-resize").click(function () {
         $("body").css("cursor", "sw-resize");
+        $(".cursor-label").text("cursor: sw-resize;");
     });
     $("#cur-text").click(function () {
         $("body").css("cursor", "text");
+        $(".cursor-label").text("cursor: text;");
     });
     $("#cur-w-resize").click(function () {
         $("body").css("cursor", "w-resize");
+        $(".cursor-label").text("cursor: w-resize;");
     });
     $("#cur-wait").click(function () {
         $("body").css("cursor", "wait");
+        $(".cursor-label").text("cursor: wait;");
     });
     $("#cur-zoom-in").click(function () {
         $("body").css("cursor", "zoom-in");
+        $(".cursor-label").text("cursor: zoom-in;");
     });
     $("#cur-zoom-out").click(function () {
         $("body").css("cursor", "zoom-out");
+        $(".cursor-label").text("cursor: zoom-out;");
     });
 });
-
